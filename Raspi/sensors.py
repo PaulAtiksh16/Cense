dd88499d9995c4bc642e9c106caa86642f788c36
PaulAtiksh16 <75923374+PaulAtiksh16@users.noreply.github.com>
2023-05-13 15:32:06 -0700

    2 sensors somewhat working

--- Raspi/sensors.py
+++ Raspi/sensors.py
@@ -1,25 +1,49 @@
 import RPi.GPIO as GPIO
+from gpiozero import MotionSensor
+import time
+import threading
+
+GPIO.setmode(GPIO.BOARD)
+
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(4, GPIO.IN)
+
+pir = MotionSensor(4)
+pir2 = MotionSensor(14)
+
+print("bwo")
 
 # Set up GPIO pins
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(18, GPIO.IN) # sensor pin
 
 # Initialize counter
 counter = 0
 
 # Define a function to read the sensor value and update the counter
 # CHANGE THIS FUNCTION TO INCREMENT/DECREMENT BASED OFF SENSORS
-def update_counter(channel):
+def increment_counter():
     global counter
-    if GPIO.input(channel):
+    if pir.wait_for_motion():
         counter += 1
-    else:
+    pir.wait_for_no_motion()
+    print(counter)
+    
+def decrement_counter():
+    global counter
+    if pir2.wait_for_motion():
         counter -= 1
+    pir.wait_for_no_motion()
+    print(counter)
+    
+increment_thread = threading.Thread(target=increment_counter)
+decrement_thread = threading.Thread(target=decrement_counter)
 
 # Add an event listener for the sensor pin
-GPIO.add_event_detect(18, GPIO.BOTH, callback=update_counter)
+# GPIO.add_event_detect(4, GPIO.BOTH, callback=update_counter)
 
 # Run an infinite loop to keep the script running
 while True:
-    # Do any other processing here
-    pass
+#     increment_counter()
+#     decrement_counter()
+    increment_thread.start()
+    decrement_thread.start()
