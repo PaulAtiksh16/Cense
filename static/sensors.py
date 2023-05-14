9cd607e6643d924e423dc94943b16f7038a5c198
PaulAtiksh16 <75923374+PaulAtiksh16@users.noreply.github.com>
2023-05-14 16:40:34 -0700

    both sensors working

--- static/sensors.py
+++ static/sensors.py
@@ -6,21 +6,23 @@ from gpiozero import MotionSensor
 counter = 0
 
 # Initialize motion sensors
-pir = MotionSensor(4)
-# pir2 = MotionSensor(14)
+pir = MotionSensor(4) #blue/white sensor: 4
+pir2 = MotionSensor(17) #green sensor: 17
 
 # Define a function to read the sensor value and update the counter
 def update_counter():
     global counter
     if pir.value == 1:
         counter += 1
-#     elif pir2.value == 1:
-#         counter -= 1
+    if pir2.value == 1:
+        counter -= 1
+    if counter < 0:
+        counter = 0
     print(counter)
 
 # Add an event listener for the sensor pins
 pir.when_motion = update_counter
-# pir2.when_motion = update_counter
+pir2.when_motion = update_counter
 
 
 app = Flask(__name__, template_folder='/home/admin/Desktop/Cense2.0/templates')
